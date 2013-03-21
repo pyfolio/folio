1734d48ad32f1cfd7a0203b9f64fedfb22caccc6
Juan M <jm@guide42.com>
2013-03-21 19:04:12 +0100

    Manually create folio in tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,16 +7,15 @@ import unittest
 
 class FolioTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.proj = folio.Folio(__name__)
-        self.proj.config['TESTING'] = True
+    def _create_folio(self, **kwargs):
+        proj = folio.Folio(__name__, **kwargs)
+        proj.config['TESTING'] = True
 
-    def tearDown(self):
-        self.proj = None
+        return proj
 
     def test_default_builders(self):
         expected = ['*', '*.html']
-        actual = [pattern for pattern, _ in self.proj.builders]
+        actual = [pattern for pattern, _ in self._create_folio().builders]
 
         # Compare only the patterns because the implementation of the builder
         # can change with the time.
@@ -26,68 +25,79 @@ class FolioTestCase(unittest.TestCase):
         cwd = os.path.abspath(os.path.dirname(__file__))
         msg = 'Import path must be the absolute path of directory where the' \
               ' project script is.'
-        self.assertEquals(cwd, self.proj.import_path, msg)
+        self.assertEquals(cwd, self._create_folio().import_path, msg)
 
     def test_register_extension(self):
         import fixtures.ext
 
-        with self.assertRaises(fixtures.ext.ExtensionRegistered):
-            self.proj.register_extension(fixtures.ext)
+        proj = self._create_folio()
 
-        self.assertIn('ext', self.proj.extensions)
+        with self.assertRaises(fixtures.ext.ExtensionRegistered):
+            proj.register_extension(fixtures.ext)
+
+        self.assertIn('ext', proj.extensions)
 
         with self.assertRaises(LookupError):
-            self.proj.register_extension(fixtures.ext)
+            proj.register_extension(fixtures.ext)
 
     def test_register_extension_invalid(self):
         ext = {'register': lambda: None}
         with self.assertRaises(ValueError):
-            self.proj.register_extension(ext)
+            self._create_folio().register_extension(ext)
 
     def test_add_builder_basestring(self):
-        self.proj.add_builder('test', lambda: None)
-        self.assertIn('test', [pattern for pattern, _ in self.proj.builders])
+        proj = self._create_folio()
+        proj.add_builder('test', lambda: None)
+
+        self.assertIn('test', [pattern for pattern, _ in proj.builders])
 
     def test_add_builder_iterator(self):
-        self.proj.add_builder(['one', 'two'], lambda: None)
+        proj = self._create_folio()
+        proj.add_builder(['one', 'two'], lambda: None)
 
-        patterns = [pattern for pattern, _ in self.proj.builders]
+        patterns = [pattern for pattern, _ in proj.builders]
 
         self.assertIn('one', patterns)
         self.assertIn('two', patterns)
 
     def test_add_builder_not_callable(self):
         with self.assertRaises(TypeError):
-            self.proj.add_builder('hello.html', 'hello')
+            self._create_folio().add_builder('hello.html', 'hello')
 
     def test_add_builder_not_iterable(self):
         with self.assertRaises(TypeError):
-            self.proj.add_builder(lambda: None, lambda: None)
+            self._create_folio().add_builder(lambda: None, lambda: None)
 
     def test_get_builder(self):
-        static_builder = self.proj.get_builder('*')
-        template_builder = self.proj.get_builder('*.html')
+        proj = self._create_folio()
+
+        static_builder = proj.get_builder('*')
+        template_builder = proj.get_builder('*.html')
 
         self.assertEquals(folio._static_builder, static_builder)
         self.assertEquals(folio._template_builder, template_builder)
 
     def test_get_builder_not_found(self):
-        self.proj.builders = []
-        self.assertEquals(None, self.proj.get_builder('foobar'))
+        proj = self._create_folio()
+        proj.builders = []
+
+        self.assertEquals(None, proj.get_builder('foobar'))
 
     def test_is_template(self):
-        self.assertTrue(self.proj.is_template('index.html'))
-        self.assertTrue(self.proj.is_template('good_news/im_alive.html'))
-        self.assertTrue(self.proj.is_template('blog/20130228-helloworld.md'))
+        proj = self._create_folio()
 
-        self.assertFalse(self.proj.is_template('.hidden'))
-        self.assertFalse(self.proj.is_template('_draft-conquer_the_world.md'))
-        self.assertFalse(self.proj.is_template('projects/.hidden'))
-        self.assertFalse(self.proj.is_template('projects/_secret.html'))
-        self.assertFalse(self.proj.is_template('projects/_inc/unique.css'))
-        self.assertFalse(self.proj.is_template('_inc/reset.css'))
-        self.assertFalse(self.proj.is_template('_inc/_underscore2.js'))
-        self.assertFalse(self.proj.is_template('.git/config'))
+        self.assertTrue(proj.is_template('index.html'))
+        self.assertTrue(proj.is_template('good_news/im_alive.html'))
+        self.assertTrue(proj.is_template('blog/20130228-helloworld.md'))
+
+        self.assertFalse(proj.is_template('.hidden'))
+        self.assertFalse(proj.is_template('_draft-conquer_the_world.md'))
+        self.assertFalse(proj.is_template('projects/.hidden'))
+        self.assertFalse(proj.is_template('projects/_secret.html'))
+        self.assertFalse(proj.is_template('projects/_inc/unique.css'))
+        self.assertFalse(proj.is_template('_inc/reset.css'))
+        self.assertFalse(proj.is_template('_inc/_underscore2.js'))
+        self.assertFalse(proj.is_template('.git/config'))
 
 
 if __name__ == '__main__':
